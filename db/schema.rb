6c22f79a5ada74c286d8ccb4c5c2f9b95e2f21a6
Jacob Wheeler <wheeler.jacob.r@gmail.com>
2018-06-26 15:03:07 -0600

    add base price to cabin
    Reviewers: #booking_engine, yasso1am
    
    Reviewed By: #booking_engine, yasso1am
    
    Subscribers: yasso1am
    
    Differential Revision: http://phabricator.devpointstudios.com/D2158

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180626183324) do
+ActiveRecord::Schema.define(version: 20180626210042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180626183324) do
     t.boolean "ada_accessible"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "base_price"
   end
 
   create_table "promo_codes", force: :cascade do |t|
