45bb2932e87a44966e3a0ff5bf4725f6021dab09
Kira Kaur <kdeep97@gmail.com>
2018-06-27 15:23:28 -0600

    add routes under api namespace for all controllers
    Reviewers: #booking_engine, jacob.wheeler
    
    Reviewed By: #booking_engine, jacob.wheeler
    
    Differential Revision: http://phabricator.devpointstudios.com/D2218

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
-    #API ROUTES SHOULD GO HERE
+    resources :cabins
+    resources :promo_codes
+    resources :reservations
+    resources :user_promo_codes
   end
 
   #Do not place any routes below this one
