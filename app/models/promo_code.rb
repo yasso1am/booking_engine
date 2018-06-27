0714524df40311e9733ba4610d1bf11af2924e4c
Kira Kaur <kdeep97@gmail.com>
2018-06-27 09:15:42 -0600

    promo code model tests all passing
    Reviewers: #booking_engine, spencer.richards
    
    Reviewed By: #booking_engine, spencer.richards
    
    Subscribers: spencer.richards
    
    Differential Revision: http://phabricator.devpointstudios.com/D2203

--- app/models/promo_code.rb
+++ app/models/promo_code.rb
@@ -1,4 +1,7 @@
 class PromoCode < ApplicationRecord
   has_many :user_promo_codes, dependent: :destroy
   has_many :users, through: :user_promo_codes 
+  validates :code, :description, :start_date, :end_date, :max_useable, presence: true
+  validates :max_useable, numericality: { only_integer: true }
+  validates :code, uniqueness: true
 end
--- spec/models/promo_code_spec.rb
+++ spec/models/promo_code_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe PromoCode, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	describe 'attributes' do
+		[:code, :description, :max_useable, :start_date, :end_date].each { |attr| it { should respond_to attr }}
+	end
+
+	describe 'validations' do
+		it { should validate_presence_of(:code) }
+		it { should validate_presence_of(:description) }
+		it { should validate_presence_of(:max_useable) }
+		it { should validate_presence_of(:start_date) }
+		it { should validate_presence_of(:end_date) }
+		it { should validate_numericality_of(:max_useable) }
+		it { should validate_uniqueness_of(:code) }
+	end
+	
+	describe 'relationships' do
+   	it { should have_many(:user_promo_codes).dependent(:destroy) }
+   	it { should have_many(:users).through(:user_promo_codes) }
+  end
 end
