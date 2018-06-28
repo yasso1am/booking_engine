b10d22419cffa3239b63f89b5379cc0f12b82300
Bianca Hale <sashabtt@gmail.com>
2018-06-28 14:10:31 -0600

    testing promo code controller
    Summary: all tests passing!
    
    Reviewers: #booking_engine, anthonymkelley
    
    Reviewed By: #booking_engine, anthonymkelley
    
    Subscribers: derekdapp
    
    Differential Revision: http://phabricator.devpointstudios.com/D2224

--- app/controllers/api/promo_codes_controller.rb
+++ app/controllers/api/promo_codes_controller.rb
@@ -1,2 +1,43 @@
 class Api::PromoCodesController < ApplicationController
+  before_action :set_promo_code, only:  [:show, :update, :destroy]
+
+  def index
+   render json: PromoCode.all
+  end
+
+  def show
+    render json: @promo_code
+  end
+
+  def create
+    promo_code = PromoCode.create(promo_code_params)
+    if promo_code.save
+      render json: promo_code
+    else
+      render json: {errors: promo_code.errors.full_messages.join(',')}, status: 422
+    end
+  end
+
+  def update
+    if @promo_code.update(promo_code_params)
+      render json: @promo_code
+    else
+      render json: {errors: @promo_code.errors.full_messages.join(',')}, status: 422
+    end
+  end
+
+  def destroy
+    @promo_code.destroy
+  end
+
+  private
+  def set_promo_code
+    @promo_code = PromoCode.find(params[:id])
+  end
+
+  def promo_code_params
+    params.require(:promo_code).permit(:code, :description, :start_date, :end_date, :max_useable, :kind, :value)
+  end
+
 end
+
--- spec/controllers/api/promo_codes_controller_spec.rb
+++ spec/controllers/api/promo_codes_controller_spec.rb
@@ -2,4 +2,72 @@ require 'rails_helper'
 
 RSpec.describe Api::PromoCodesController, type: :controller do
 
+
+  let (:valid_attributes) {
+    {code: "123", description: "test", start_date: '2018-09-03', end_date: '2018-09-10', max_useable: 2, kind: 'summer', value: 7}
+  }
+
+  let (:invalid_attributes) {
+    {code: 123, description: "test", start_date: '2018-09-03' , end_date: '2018-09-10', max_useable: 2, kind: 'summer', value: 7}
+
+  }
+
+  describe "GET #index" do
+    it "returns success" do
+      promo_code = FactoryBot.create(:promo_code)
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #show" do
+    it "returns success" do
+      promo_code = FactoryBot.create(:promo_code)
+      get :show, params: {id: promo_code.id}
+      expect(response).to be_successful
+    end
+  end
+
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "returns success" do
+        expect {
+          post :create, params: { promo_code: valid_attributes }
+        }.to change(PromoCode, :count).by(1)
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "with new info" do
+      let(:new_attributes) {
+        {max_useable: 1}
+      }
+      it "updates the promo code" do
+        promo_code = FactoryBot.create(:promo_code)
+        put :update, params: {id: promo_code.id, promo_code: new_attributes}
+        promo_code.reload
+        expect(promo_code.max_useable).to eql(new_attributes[:max_useable])
+      end
+
+      context "with invalid params" do
+        it 'does not update the promo code' do
+        promo_code = FactoryBot.create(:promo_code)
+        put :update, params: {id: promo_code.id, promo_code: invalid_attributes}
+        promo_code.reload
+        expect(promo_code.code).to_not eql(invalid_attributes[:code])
+        end
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroys the promo code" do
+      promo_code = FactoryBot.create(:promo_code)
+      expect {
+        delete :destroy, params: {id: promo_code.id}
+      }.to change(PromoCode, :count).by(-1)
+    end
+  end
 end
