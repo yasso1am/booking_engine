b78178e8b08db1188e482a1aaf528185ee81058f
Robin Green <greenrj4@gmail.com>
2018-06-28 17:08:26 -0600

    404
    Reviewers: #booking_engine, anthonymkelley
    
    Reviewed By: #booking_engine, anthonymkelley
    
    Differential Revision: http://phabricator.devpointstudios.com/D2242

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { 
+  Header, 
+  Image, 
+  } from 'semantic-ui-react';
 
-class NoMatch extends Component {
-  render() {
-    return (
-      <Header as='h1' textAlign='center'>
-        Page Not Found
-        <Link to='/'> Home</Link>
-      </Header>
+  
+  class NoMatch extends Component {
+    render() {
+      return (
+        <div>
+          <Header as='h1' textAlign='center'>
+          I Got You This Time Buzz!!
+          (Page Not Found)
+          <br/>
+          <Link to='/'>Come Home</Link>
+          <hr/>
+        </Header>
+          <Image src='https://media.giphy.com/media/ZmagpA1sYZC3m/giphy.gif' height="50%" width="50%" aline centered />
+        </div>
     );
   }
+
 }
 
 export default NoMatch;
+
+
+
