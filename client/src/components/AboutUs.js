0ccba8089ed05e05e4fec34c0aeab96d6167b758
Kira Kaur <kdeep97@gmail.com>
2018-06-29 17:27:07 -0600

    page is responsive and uses grid
    Reviewers: #booking_engine, anthonymkelley
    
    Reviewed By: #booking_engine, anthonymkelley
    
    Differential Revision: http://phabricator.devpointstudios.com/D2258

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,58 +1,90 @@
 import React, { Component } from 'react';
-import { Divider, Container, Button } from 'semantic-ui-react';
+import { Divider, Container, Button, Grid } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 class AboutUs extends Component {
 	render() {
 		return(
 			<div>
-				<MainHeader></MainHeader>
-				<Container>
-					<Divider hidden />
-					<Header>About Us</Header>
-					<Paragraph>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla feugiat ac nulla in varius. 
-						Cras in dapibus dui. Aenean sit amet posuere sapien. Integer mattis quam sed maximus venenatis. 
-						Fusce vitae enim ipsum. Pellentesque at tellus ac turpis bibendum vehicula. Morbi pulvinar suscipit semper. 
-						Ut vestibulum gravida enim in varius. Nulla in facilisis sapien. Phasellus at nulla sed ante vehicula facilisis. 
-						Aliquam risus odio, eleifend sit amet posuere eu, blandit vel orci.
-					</Paragraph>
-					<Divider segment />
-					<Header>Our Mission</Header>
-					<Paragraph>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla feugiat ac nulla in varius. 
-						Cras in dapibus dui. Aenean sit amet posuere sapien. Integer mattis quam sed maximus venenatis. 
-						Fusce vitae enim ipsum. Pellentesque at tellus ac turpis bibendum vehicula. Morbi pulvinar suscipit semper. 
-						Ut vestibulum gravida enim in varius. Nulla in facilisis sapien. Phasellus at nulla sed ante vehicula facilisis. 
-						Aliquam risus odio, eleifend sit amet posuere eu, blandit vel orci.
-					</Paragraph>
-					<Divider hidden />
-					<InfoContainer>
-						<InfoBubble>50 Cabins</InfoBubble>
-						<InfoBubble>5 Stars</InfoBubble>
-						<InfoBubble>100 Acres</InfoBubble>
-						<InfoBubble>10 Buildings</InfoBubble>
-					</InfoContainer>
-					<Divider hidden />
-				</Container>
+				<Grid stackable>
+					<Grid.Row>
+						<Grid.Column mobile={4} computer={16}>
+							<MainHeader></MainHeader>
+						</Grid.Column>
+					</Grid.Row>
+						<Container>
+							<Grid.Row>
+								<Grid.Column mobile={4} tablet={8} computer={16}>
+									<Divider hidden />
+									<Header>Our History</Header>
+									<Paragraph>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla feugiat ac nulla in varius. 
+										Cras in dapibus dui. Aenean sit amet posuere sapien. Integer mattis quam sed maximus venenatis. 
+										Fusce vitae enim ipsum. Pellentesque at tellus ac turpis bibendum vehicula. Morbi pulvinar suscipit semper. 
+										Ut vestibulum gravida enim in varius. Nulla in facilisis sapien. Phasellus at nulla sed ante vehicula facilisis. 
+										Aliquam risus odio, eleifend sit amet posuere eu, blandit vel orci.
+									</Paragraph>
+								</Grid.Column>
+							</Grid.Row>
+							<Grid.Row>
+								<Grid.Column mobile={4} tablet={8} computer={16}>
+									<Divider segment />
+									<Header>Our Mission</Header>
+									<Paragraph>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla feugiat ac nulla in varius. 
+										Cras in dapibus dui. Aenean sit amet posuere sapien. Integer mattis quam sed maximus venenatis. 
+										Fusce vitae enim ipsum. Pellentesque at tellus ac turpis bibendum vehicula. Morbi pulvinar suscipit semper. 
+										Ut vestibulum gravida enim in varius. Nulla in facilisis sapien. Phasellus at nulla sed ante vehicula facilisis. 
+										Aliquam risus odio, eleifend sit amet posuere eu, blandit vel orci.
+									</Paragraph>
+									<Divider hidden />
+								</Grid.Column>
+							</Grid.Row>
+							<Grid.Row>
+								<Divider segment />
+								<Header>Features</Header>
+								<Grid.Column mobile={4} tablet={8} computer={16}>
+									<InfoContainer>
+										<InfoBubble>50 Cabins</InfoBubble>
+										<InfoBubble>5 Stars</InfoBubble>
+										<InfoBubble>100 Acres</InfoBubble>
+										<InfoBubble>10 Buildings</InfoBubble>
+									</InfoContainer>
+									<Divider hidden />
+								</Grid.Column>
+							</Grid.Row>
+							<Grid.Row>
+								<Grid.Column mobile={4} tablet={8} computer={16}>
+									<Divider segment />
+									<Header>What To Expect</Header>
+									<Paragraph>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla feugiat ac nulla in varius. 
+										Cras in dapibus dui. Aenean sit amet posuere sapien. Integer mattis quam sed maximus venenatis. 
+										Fusce vitae enim ipsum. Pellentesque at tellus ac turpis bibendum vehicula. Morbi pulvinar suscipit semper. 
+										Ut vestibulum gravida enim in varius. Nulla in facilisis sapien. Phasellus at nulla sed ante vehicula facilisis. 
+										Aliquam risus odio, eleifend sit amet posuere eu, blandit vel orci.
+									</Paragraph>
+									<Divider hidden />
+								</Grid.Column>
+							</Grid.Row>
+						</Container>
+				</Grid>
 			</div>
 		)
 	}
 }
 
 const MainHeader = styled.div`
-	background-image: url(https://images.unsplash.com/photo-1488441770602-aed21fc49bd5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f093285a418b6aadfc00bd4b8354ec02&auto=format&fit=crop&w=1350&q=80);
+	background-image: url(https://images.unsplash.com/photo-1488441770602-aed21fc49bd5?ixlib=rb-0.3.5&s=07ba5eba4f4a1d7963d1b9cbf00e5667&auto=format&fit=crop&w=2550&q=80);
 	background-size: cover;
 	background-position: center center;
 	background-repeat: no-repeat; 
 	height: 450px;
-	display: flex;
-	align-items: center;
-	justify-content: center;
 `
 const Paragraph = styled.p`
 	text-align: center;
 	font-family: "Palatino Linotype", "Book Antiqua", Palatino, serif;
+	padding: 15px;
 `
 const Header = styled.h3`
 	text-align: center;
